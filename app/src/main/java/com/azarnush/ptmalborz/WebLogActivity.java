136f13471f04e10eab7883860410f388e807028f
mary azarnush <mary136804@gmail.com>
2020-01-27 13:17:14 +0330

    اپدیت اول

---
+++ app/src/main/java/com/azarnush/ptmalborz/WebLogActivity.java
@@ -0,0 +1,18 @@
+package com.azarnush.ptmalborz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_log);
+
+        WebView myWebView = (WebView) findViewById(R.id.wv_weblog);
+        myWebView.loadUrl("https://webeskan.com/Blog");
+    }
+}
