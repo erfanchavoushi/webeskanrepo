b25b1fbd160b4401fabb040ef5542658e6a83bb7
mary azarnush <mary136804@gmail.com>
2020-02-16 12:59:27 +0330

    11/26

--- app/src/main/java/com/azarnush/ptmalborz/WebLogFragment.java
+++ app/src/main/java/com/azarnush/ptmalborz/WebLogFragment.java
@@ -5,13 +5,18 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebLogFragment extends Fragment {
+    ProgressDialog progressDialog;
 
     @Nullable
     @Override
@@ -20,6 +25,27 @@ public class WebLogFragment extends Fragment {
         HomeActivity.toolbar.setTitle("وبلاگ");
 
         WebView myWebView = root.findViewById(R.id.wv_weblog);
+        progressDialog = new ProgressDialog(getContext());
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressDialog.setMessage("در حال بارگذاری وبلاگ");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
+
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressDialog.dismiss();
+
+                super.onPageFinished(view, url);
+            }
+        });
+
         myWebView.loadUrl("https://webeskan.com/Blog");
         return root;
     }
